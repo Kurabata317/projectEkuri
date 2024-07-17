d6c3f36e59eec84d326bae7b1e3d7d059ce868da
Kurabata317 <acayotalini@gmail.com>
2024-07-18 01:31:20 +0900

    bugfix

--- bot.py
+++ bot.py
@@ -80,6 +80,7 @@ async def add_buttons_to_message(message, author_id):
     view = discord.ui.View()
 
     matches = twitter_url_pattern.finditer(message.content)
+    buttons = []  # 버튼을 담을 리스트
     
     for match in matches:
         username_and_path = match.group(2)  # 사용자 이름 및 경로 추출
@@ -112,14 +113,17 @@ async def add_buttons_to_message(message, author_id):
             callback=delete_message
         )
         
-        # 링크 버튼과 삭제 버튼을 뷰에 추가
-        view.add_item(twitter_button)
-        view.add_item(x_button)
-        view.add_item(delete_button)
+        # 버튼을 리스트에 추가
+        buttons.append(twitter_button)
+        buttons.append(x_button)
+        buttons.append(delete_button)
+    
+    # 뷰에 버튼들을 추가
+    for button in buttons:
+        view.add_item(button)
 
     # 메시지에 뷰 추가
     await message.edit(view=view)
-
 
 # config.json에서 봇 토큰을 불러오는 함수
 def load_config():
